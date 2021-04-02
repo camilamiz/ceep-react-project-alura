6668d761f758d8fe0c3960470dca8ad18bad56c8
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-04-01 22:03:24 -0300

    add form component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Form extends Component {
+  render() {
+    return(
+      <form className="form-cadastro">
+        <input
+          type="text"
+          placeholder="Title"
+          className="form_input"
+        />
+        <textarea
+          placeholder="Please write your note..."
+          className="form_input"
+        />
+        <button className="form_input form_submit">
+          Create note
+        </button>
+      </form>
+    );
+  }
+
+}
+
+export default Form;
